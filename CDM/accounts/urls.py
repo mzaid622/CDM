0a0e4cdb0dcf12939f427a75b698ce4b8c47c0b2
mzaid622 <M.Zaid990447@gmail.com>
2026-01-05 17:58:55 +0500

    Curd Operation Implement

--- CDM/accounts/urls.py
+++ CDM/accounts/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.home,name="home"), 
-  path("products/", views.products,name="products"),
-  path("customer/<str:pk_id>",views.customer,name="customer"),
-  ]
+    path("", views.home, name="home"),
+    path("products/", views.products, name="products"),
+    path("customer/<str:pk_id>", views.customer, name="customer"),
+    path("create_order", views.createorder, name="create_order"),
+    path("edit_order/<str:pk_id>",views.editorder,name="edit_order"),
+    path("delete_order/<str:pk_id>",views.deleteorder,name="delete_order"),
+]
+
--- CDM/accounts/views.py
+++ CDM/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+# from django.http import HttpResponse
 from .models import *
+from .form import OrderForm
 
 # Create your views here.
 
@@ -36,3 +37,46 @@ def customer(request,pk_id):
 
     context = {"cust":cust,"order":order,"order_count":order_count}
     return render(request, "accounts/customer.html",context)
+
+
+def createorder(request):
+
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = OrderForm()
+
+    context = {"form":form}
+    return render(request,"accounts/order_form.html",context)
+
+
+def editorder(request,pk_id):
+    order = Order.objects.get(id=pk_id)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    else:
+        form = OrderForm(instance = order)
+
+    context = {"form":form}
+    return render(request,"accounts/order_form.html",context)
+
+
+def deleteorder(request,pk_id):
+    item = Order.objects.get(id=pk_id)
+    item_n=item.product
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+
+
+    context = {"item":item_n}
+    return render(request,"accounts/delete_form.html",context)
